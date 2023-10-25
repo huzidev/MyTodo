98443f5fde78adda081c175986da0803318dade0
huzidev <huzaifa.iqdev@gmail.com>
2023-10-25 19:57:47 +0500

    Verify middle-ware

--- server/routes/user/index.ts
+++ server/routes/user/index.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { Response } from "express";
+import express from "express";
 const router = express.Router();
 const Verification = require("@app/Middleware/Verification");
 const UserController = require("@app/Controllers/UserController");
@@ -12,7 +12,4 @@ router.use(
 
 router.post("/contact", UserController.Contact);
 router.put("/updateuser/:id", Verification, UserController.Update);
-
-router.get("/about", Verification, (req: any, res: Response) => {
-  return res.json(req.userInfo);
-});
+router.get("/about", Verification, UserController.About);
