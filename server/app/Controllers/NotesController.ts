85d0f4d17063a3757a1f6d0dc2361df51bf11c31
huzidev <huzaifa.iqdev@gmail.com>
2023-10-25 20:03:33 +0500

    import alias controller

--- server/app/Controllers/NotesController.ts
+++ server/app/Controllers/NotesController.ts
@@ -2,40 +2,40 @@ import { Response } from "express";
 const Contact = require("@app/Models/User");
 const Notes = require("@app/Models/Notes");
 const User = require("@app/Models/User");
+const CompletedNotes = require("@app/Models/completedNotes");
 
 const NotesController = {
-    // Get All Notes
+  // Get All Notes
   GetNotes: async (req: any, res: Response) => {
-  try {
-    if (req.path.includes("mystring")) {
-      res.send('The current pathname includes "mystring"');
+    try {
+      if (req.path.includes("mystring")) {
+        res.send('The current pathname includes "mystring"');
+      }
+      const notes = await Notes.find({ user: req.userID });
+      res.json(notes);
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      } else {
+        console.error(String(err));
+      }
+      return res.status(500).send("Failed! Internal Server Error");
     }
-    const notes = await Notes.find({ user: req.userID });
-    res.json(notes);
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error(err.message);
-    } else {
-      console.error(String(err));
-    }
-    return res.status(500).send("Failed! Internal Server Error");
-  }
-},
+  },
 
-GetCompletedNotes: async (req: any, res) => {
-  try {
-    const notes = await CompletedNotes.find({ user: req.userID });
-    res.json(notes);
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error(err.message);
-    } else {
-      console.error(String(err));
+  GetCompletedNotes: async (req: any, res: Response) => {
+    try {
+      const notes = await CompletedNotes.find({ user: req.userID });
+      res.json(notes);
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      } else {
+        console.error(String(err));
+      }
+      return res.status(500).send("Failed! Internal Server Error");
     }
-    return res.status(500).send("Failed! Internal Server Error");
-  }
-}
+  },
 };
 
 module.exports = NotesController;
- 
