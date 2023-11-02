0d8dd87eb0921d70933ebd949884a2a2db8d7118
huzidev <huzaifa.iqdev@gmail.com>
2023-11-02 14:06:13 +0500

    import ref

--- client/src/Router/index.tsx
+++ client/src/Router/index.tsx
@@ -1,8 +1,8 @@
 import "antd/dist/antd.css";
+import UserState from "context/UserState";
 import NotFoundPage from "pages/NotFound/Error";
+import PageWrapper from "pages/PageWrapper";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import UserState from "../context/UserState";
-import PageWrapper from "../pages/Home/Home";
 import routes from "./routes";
 export default function AppRouter(): JSX.Element {
   return (
