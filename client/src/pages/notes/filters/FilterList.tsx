f1cc9a56b5d206f32735e3451626f123057cdc03
huzidev <huzaifa.iqdev@gmail.com>
2023-11-02 21:30:05 +0500

    notes state for filters

--- client/src/pages/notes/filters/FilterList.tsx
+++ client/src/pages/notes/filters/FilterList.tsx
@@ -1,152 +1,147 @@
-import Fade from '@mui/material/Fade';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { Button, Col, Row, Typography } from 'antd';
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import ShowNotes from '../notes/ShowNotes';
-// const NoteState = lazy(() =>
-//     new Promise((resolve) => setTimeout(resolve, 1000)).then(() => import('../notes/NoteState'))
-// );
-import NoteState from '../notes/NoteState';
+import Fade from "@mui/material/Fade";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { Button, Col, Row, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import ShowNotes from "../notes/ShowNotes";
 
 export default function FilterList(props: any): JSX.Element {
-    const Location = useLocation();
-    let allNotes = props.notes;
+  const Location = useLocation();
+  let allNotes = props.notes;
 
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [state, setState] = useState<boolean | null>(null);
-    const [items, setItems] = useState(allNotes);
-    // const [message, setMessageState] = useState<string | null>('');
-    
-    const addNotesPath: boolean = Location.pathname.includes('/addnote');
-    const completedNotesPath: boolean = Location.pathname.includes('/completed');
-    const locationName: string = addNotesPath ? "/allnotes" : "/completednotes"
-    
-    useEffect(() => {
-        // so state will changed to default form and allNotes will be fetched
-        setState(null);
-    }, [locationName])
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [state, setState] = useState<boolean | null>(null);
+  const [items, setItems] = useState(allNotes);
+  // const [message, setMessageState] = useState<string | null>('');
 
-    // MUI
-    const open = Boolean(anchorEl);
-    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(e.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const addNotesPath: boolean = Location.pathname.includes("/addnote");
+  const completedNotesPath: boolean = Location.pathname.includes("/completed");
+  const locationName: string = addNotesPath ? "/allnotes" : "/completednotes";
 
-    // when user clicked on filter list then state will change to true & all the notes with that specific catgeory will fetch only therefore state must be true but initially it is false 
-    let Data = state ? items : allNotes
+  useEffect(() => {
+    // so state will changed to default form and allNotes will be fetched
+    setState(null);
+  }, [locationName]);
 
-    const allItems = [...new Set(allNotes.map((currentEle: any) => (
-        currentEle.category
-    )))]
+  // MUI
+  const open = Boolean(anchorEl);
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    function filterItems(items: string) {
-        const updatedFilterItems = allNotes.filter((element: any) => {
-            // element.category targets category only ex: grocery, payments and bills
-            return element.category === items
-        })
-        setItems(updatedFilterItems);
-        setState(true);
-    }
+  // when user clicked on filter list then state will change to true & all the notes with that specific catgeory will fetch only therefore state must be true but initially it is false
+  let Data = state ? items : allNotes;
 
-    const Text = addNotesPath ? "Yours Notes" : "Completed Notes";
-    let Type = addNotesPath ? "Added" : "Completed";
-    // let testMess: any;
-    
-    // useEffect(() => {
-    //     testMess = null;
-    //     testMess = addNotesPath ? "No Task Added" : "No Task Completed";
-    //     console.log("what is message type?", testMess);
-    //     setMessageState(null);
-    //     setTimeout(() => {
-    //         setMessageState(testMess);
-    //     }, 5000)
-    // }, [Location.pathname])
+  const allItems = [
+    ...new Set(allNotes.map((currentEle: any) => currentEle.category)),
+  ];
 
-    // status will either be completely true or completely false
-    let status: boolean | undefined;
-    allNotes.forEach((Element: any, i: number) => {
-        for (let key in Element) {
-            status = Element.isCompleted
-        }
+  function filterItems(items: string) {
+    const updatedFilterItems = allNotes.filter((element: any) => {
+      // element.category targets category only ex: grocery, payments and bills
+      return element.category === items;
     });
+    setItems(updatedFilterItems);
+    setState(true);
+  }
 
-    const error = status === undefined;
-    const style = {
-        display: "flex",
-        justifyContent: "space-between",
+  const Text = addNotesPath ? "Yours Notes" : "Completed Notes";
+  let Type = addNotesPath ? "Added" : "Completed";
+  // let testMess: any;
+
+  // useEffect(() => {
+  //     testMess = null;
+  //     testMess = addNotesPath ? "No Task Added" : "No Task Completed";
+  //     console.log("what is message type?", testMess);
+  //     setMessageState(null);
+  //     setTimeout(() => {
+  //         setMessageState(testMess);
+  //     }, 5000)
+  // }, [Location.pathname])
+
+  // status will either be completely true or completely false
+  let status: boolean | undefined;
+  allNotes.forEach((Element: any, i: number) => {
+    for (let key in Element) {
+      status = Element.isCompleted;
     }
+  });
 
-    return (
-        <div>
-            {
-                <>
-                    {
-                        (completedNotesPath && (status !== true || error)) || (addNotesPath && (status !== false || error)) ? (
-                            // <Typography.Title level={4}>
-                            //     {/* { message === null ? <Spin size='large' /> : message } */}
+  const error = status === undefined;
+  const style = {
+    display: "flex",
+    justifyContent: "space-between",
+  };
 
-                            // </Typography.Title>
-                            // <Suspense fallback={<div><Spin size='large' /></div>}>
-                            //     {type !== '' && <NoteState type={type} />}
-                            // </Suspense>
-                            <NoteState type={Type} />
-                        ) : (
-                            <div style={style}>
-                                <Typography.Title level={4}>
-                                    {Text}
-                                </Typography.Title>
-                                <div style={style}>
-                                    <Button
-                                        type="ghost"
-                                        className='Dark'
-                                        onClick={handleClick}
-                                    >
-                                        Filter the list
-                                    </Button>
-                                    <Menu
-                                        id="fade-menu"
-                                        MenuListProps={{
-                                            'aria-labelledby': 'fade-button',
-                                        }}
-                                        anchorEl={anchorEl}
-                                        open={open}
-                                        onClose={handleClose}
-                                        TransitionComponent={Fade}
-                                    >
-                                        <MenuItem onClick={() => setItems(allNotes)}>All items</MenuItem>
-                                        {allItems.map((currentEle: any, index: number) => {
-                                            return (
-                                                <span key={index}>
-                                                    <MenuItem onClick={() => filterItems(currentEle)}>
-                                                        {currentEle}
-                                                    </MenuItem>
-                                                </span>
-                                            )
-                                        })}
-                                    </Menu>
-                                </div>
-                            </div>
-                        )
-                    }
-                </>
-            }
-            <Row gutter={16}>
-                {/* Data.map so if user clicked on FilterItems then only notes with specific category will be render NOT allNotes */}
-                {Data.map((note: any) => {
+  return (
+    <div>
+      {
+        <>
+          {(completedNotesPath && (status !== true || error)) ||
+          (addNotesPath && (status !== false || error)) ? (
+            // <Typography.Title level={4}>
+            //     {/* { message === null ? <Spin size='large' /> : message } */}
+
+            // </Typography.Title>
+            // <Suspense fallback={<div><Spin size='large' /></div>}>
+            //     {type !== '' && <NoteState type={type} />}
+            // </Suspense>
+            <h1>No task</h1>
+          ) : (
+            <div style={style}>
+              <Typography.Title level={4}>{Text}</Typography.Title>
+              <div style={style}>
+                <Button type="ghost" className="Dark" onClick={handleClick}>
+                  Filter the list
+                </Button>
+                <Menu
+                  id="fade-menu"
+                  MenuListProps={{
+                    "aria-labelledby": "fade-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  TransitionComponent={Fade}
+                >
+                  <MenuItem onClick={() => setItems(allNotes)}>
+                    All items
+                  </MenuItem>
+                  {allItems.map((currentEle: any, index: number) => {
                     return (
-                        !note.isCompleted && addNotesPath || note.isCompleted && completedNotesPath ? (
-                            <Col span={12} style={{ margin: '10px 0px' }}>
-                                <ShowNotes key={note._id} updateNote={props.updateNote} note={note} isCompleted={note.isCompleted} />
-                            </Col>
-                        ) : null
-                    )
-                })}
-            </Row>
-        </div>
-    )
+                      <span key={index}>
+                        <MenuItem onClick={() => filterItems(currentEle)}>
+                          {currentEle}
+                        </MenuItem>
+                      </span>
+                    );
+                  })}
+                </Menu>
+              </div>
+            </div>
+          )}
+        </>
+      }
+      <Row gutter={16}>
+        {/* Data.map so if user clicked on FilterItems then only notes with specific category will be render NOT allNotes */}
+        {Data.map((note: any) => {
+          return (!note.isCompleted && addNotesPath) ||
+            (note.isCompleted && completedNotesPath) ? (
+            <Col span={12} style={{ margin: "10px 0px" }}>
+              <ShowNotes
+                key={note._id}
+                updateNote={props.updateNote}
+                note={note}
+                isCompleted={note.isCompleted}
+              />
+            </Col>
+          ) : null;
+        })}
+      </Row>
+    </div>
+  );
 }
